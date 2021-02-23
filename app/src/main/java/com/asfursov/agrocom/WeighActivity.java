1e31886e4fabbffeee5e2c96a2c990df98610e05
Andrew <andrew.895@awok.ae>
2021-02-23 19:28:22 +0200

    Dummies for all activities added

---
+++ app/src/main/java/com/asfursov/agrocom/WeighActivity.java
@@ -0,0 +1,20 @@
+package com.asfursov.agrocom;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WeighActivity extends AppCompatActivity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weigh);
+
+        mTextView = (TextView) findViewById(R.id.text);
+
+    }
+}
