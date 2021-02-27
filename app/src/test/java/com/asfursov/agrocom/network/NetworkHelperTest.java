d33ffb3dacebb059d15159eff0d90c066c134e5d
Andrew <andrew.895@awok.ae>
2021-02-27 12:38:41 +0200

    try catch in tests do not allow asserts to run properly

--- app/src/test/java/com/asfursov/agrocom/network/NetworkHelperTest.java
+++ app/src/test/java/com/asfursov/agrocom/network/NetworkHelperTest.java
@@ -5,6 +5,10 @@ import com.asfursov.agrocom.model.UserData;
 
 import junit.framework.TestCase;
 
+import org.junit.Test;
+
+import java.io.IOException;
+
 import retrofit2.Call;
 import retrofit2.Response;
 
@@ -13,48 +17,59 @@ public class NetworkHelperTest extends TestCase {
     public static final String USERID = "c778259e-78cd-11eb-9aab-d01f30b1f1a7";
     public static final String PASSWORD = "MTIzNDU=";
 
-    public void testGetAPI() {
+    public void testGetAPI() throws IOException {
         API api = NetworkHelper.getInstance().getAPI();
         Call<UserData> call = api.login(new LoginRequest(USERID, PASSWORD));
-        try {
-            Response<UserData> response = call.execute();
-            assertEquals(200,response.code());
-            assertNotNull("Response",response);
-            assertNotNull("Response data",response.body());
-            assertTrue("User ID matched",USERID.equalsIgnoreCase(response.body().getId().toString()));
-            assertTrue("Roles not empty",response.body().getRoles().size()>0);
-        } catch (Throwable e) {
 
-        }
+        Response<UserData> response = call.execute();
+        assertEquals(200, response.code());
+        assertNotNull("Response", response);
+        assertNotNull("Response data", response.body());
+        assertTrue("User ID matched", USERID.equalsIgnoreCase(response.body().getId().toString()));
+        assertTrue("Roles not empty", response.body().getRoles().size() > 0);
+
+
     }
-    public void testNoPassword() {
+
+    public void testNoPassword() throws IOException {
         API api = NetworkHelper.getInstance().getAPI();
         Call<UserData> call = api.login(new LoginRequest(USERID, null));
-        try {
-            Response<UserData> response = call.execute();
-            assertEquals(401,response.code());
-        } catch (Throwable e) {
 
-        }
+        Response<UserData> response = call.execute();
+        assertEquals(403, response.code());
+
+
     }
-    public void testNotFound() {
+
+    @Test
+    public void testNotFound() throws IOException {
         API api = NetworkHelper.getInstance().getAPI();
         Call<UserData> call = api.login(new LoginRequest("DUMMY", null));
-        try {
-            Response<UserData> response = call.execute();
-            assertEquals(404,response.code());
-        } catch (Throwable e) {
 
-        }
+        Response<UserData> response = call.execute();
+        assertEquals("Response code", 401, response.code());
+
+
     }
-    public void testWrongPassword() {
+
+    public void testWrongPassword() throws IOException {
         API api = NetworkHelper.getInstance().getAPI();
-        Call<UserData> call = api.login(new LoginRequest(USERID,"dummy"));
-        try {
-            Response<UserData> response = call.execute();
-            assertEquals(404,response.code());
-        } catch (Throwable e) {
+        Call<UserData> call = api.login(new LoginRequest(USERID, "dummy"));
 
-        }
+        Response<UserData> response = call.execute();
+        assertEquals(403, response.code());
+
+
     }
+
+    public void testSupporting() throws IOException {
+        API api = NetworkHelper.getInstance().getAPI();
+        Call<UserData> call = api.login(new LoginRequest("3E895B4B-3F09-11EB-9C88-70C94EDD47E0", null));
+
+        Response<UserData> response = call.execute();
+        assertEquals("Response code", 401, response.code());
+
+
+    }
+
 }
