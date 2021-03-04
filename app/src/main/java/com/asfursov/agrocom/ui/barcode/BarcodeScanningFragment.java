dbb38d4489419273568c67685c9ca0158fad96ce
Andrew <andrew.895@awok.ae>
2021-03-04 19:08:04 +0200

    minor bugs fixed

--- app/src/main/java/com/asfursov/agrocom/ui/barcode/BarcodeScanningFragment.java
+++ app/src/main/java/com/asfursov/agrocom/ui/barcode/BarcodeScanningFragment.java
@@ -161,6 +161,7 @@ public class BarcodeScanningFragment extends Fragment  {
 
     @SuppressLint("MissingPermission")
     private void runScanning() throws IOException {
+        isShot = false;
         cameraSource.start(surfaceView.getHolder());
         setFlash(flashState);
     }
